326435d85af30b4d535331eb582e92d179f5a344
AbdulrahmanAlsafweh <abdulrahmansafweh@gmail.com>
2025-03-25 13:32:05 +0200

    fix drag and drop for pc

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import OurClients from '../components/OurClients/OurClients.jsx';
 import Services from '../components/Services/Services.jsx';
 import WorkRoadMap from '../components/WorkRoadmap/WorkRoadmap.jsx';
+import DragAndDrop from '../components/DragAndDrop.jsx';
 export default function Home() {
   return (
     <div className="overflow-x-clip">
       <OurClients />
+      <DragAndDrop />
       <Services />
       <WorkRoadMap />
     </div>
