666de1d9276d902e1dfdfed724f10ecc935e5380
AbdulrahmanAlsafweh <abdulrahmansafweh@gmail.com>
2025-05-03 12:52:20 +0300

    fix typo

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import OurClients from '../components/OurClients/OurClients.jsx';
 import Services from '../components/Services/Services.jsx';
-import WorkRoadMap from '../components/WorkRoadmap/WorkRoadMap.jsx';
+import WorkRoadMap from "../components/WorkRoadMap/WorkRoadMap.jsx";
 import DragAndDrop from '../components/DragAndDrop.jsx';
 import HomeBlogs from '../components/Blogs/HomeBlogs.jsx';
 export default function Home() {
