3e7db9746fa85b2bd87828a29dcdb81966594299
AbdulrahmanAlsafweh <abdulrahmansafweh@gmail.com>
2025-05-14 13:13:11 +0300

    add the domain in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["api.nicgroup.co"], // Replace with your actual backend domain
+  },
+};
 
 export default nextConfig;
